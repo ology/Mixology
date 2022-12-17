0eacc191f8f2e7522daa0048ae37a7bd436e3ce2
Gene Boggs <gene@ology.net>
2022-12-17 14:12:06 -0800

    No need for DBI now

--- lib/Mojolicious/Plugin/DB.pm
+++ lib/Mojolicious/Plugin/DB.pm
@@ -1,16 +1,10 @@
 package Mojolicious::Plugin::DB;
 use Mojo::Base 'Mojolicious::Plugin', -signatures;
 
-use DBI;
 use Mojo::SQLite;
 
 sub register {
     my ($self, $app) = @_;
-
-    $app->helper(dbh => sub {
-        my ($c) = @_;
-        return state $dbh = DBI->connect('dbi:SQLite:dbname=mixology.db');
-    });
 
     $app->helper(sqlite => sub {
         my ($c) = @_;
