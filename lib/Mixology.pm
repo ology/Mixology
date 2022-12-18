b83cae2be84af41694d9bdf1cb4d6c78dcf4940d
Gene Boggs <gene@ology.net>
2022-12-18 08:45:44 -0800

    Whitespace

--- lib/Mixology.pm
+++ lib/Mixology.pm
@@ -9,7 +9,6 @@ sub startup ($self) {
   $self->secrets($config->{secrets});
 
   my $r = $self->routes;
-
   $r->get('/')                 ->to('Main#main')             ->name('main');
   $r->get('/mix')              ->to('Main#mix')              ->name('mix');
   $r->get('/unmix')            ->to('Main#unmix')            ->name('unmix');
