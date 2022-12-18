976a30fd6bfd83f9f25057e6de747c3e769f081a
Gene Boggs <gene@ology.net>
2022-12-18 08:44:00 -0800

    Why not?

--- lib/Mixology.pm
+++ lib/Mixology.pm
@@ -10,15 +10,15 @@ sub startup ($self) {
 
   my $r = $self->routes;
 
-  $r->get('/')->to('Main#main')->name('main');
-  $r->get('/mix')->to('Main#mix')->name('mix');
-  $r->get('/unmix')->to('Main#unmix')->name('unmix');
-  $r->get('/shuffle')->to('Main#shuffle')->name('shuffle');
-  $r->get('/edit')->to('Main#edit')->name('edit');
-  $r->post('/edit')->to('Main#update')->name('update');
+  $r->get('/')                 ->to('Main#main')             ->name('main');
+  $r->get('/mix')              ->to('Main#mix')              ->name('mix');
+  $r->get('/unmix')            ->to('Main#unmix')            ->name('unmix');
+  $r->get('/shuffle')          ->to('Main#shuffle')          ->name('shuffle');
+  $r->get('/edit')             ->to('Main#edit')             ->name('edit');
+  $r->post('/edit')            ->to('Main#update')           ->name('update');
   $r->get('/delete_ingredient')->to('Main#delete_ingredient')->name('delete_ingredient');
-  $r->get('/delete_category')->to('Main#delete_category')->name('delete_category');
-  $r->post('/new_category')->to('Main#new_category')->name('new_category');
+  $r->get('/delete_category')  ->to('Main#delete_category')  ->name('delete_category');
+  $r->post('/new_category')    ->to('Main#new_category')     ->name('new_category');
 }
 
 1;
