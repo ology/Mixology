fe6c02b16d8d4e74238fc792a1ae9a228f9ac5e6
Gene Boggs <gene@ology.net>
2022-12-18 14:13:13 -0800

    No more shuffle

--- lib/Mixology.pm
+++ lib/Mixology.pm
@@ -12,7 +12,6 @@ sub startup ($self) {
   $r->get('/')                 ->to('Main#main')             ->name('main');
   $r->get('/mix')              ->to('Main#mix')              ->name('mix');
   $r->get('/unmix')            ->to('Main#unmix')            ->name('unmix');
-  $r->get('/shuffle')          ->to('Main#shuffle')          ->name('shuffle');
   $r->get('/edit')             ->to('Main#edit')             ->name('edit');
   $r->post('/edit')            ->to('Main#update')           ->name('update');
   $r->get('/delete_ingredient')->to('Main#delete_ingredient')->name('delete_ingredient');
